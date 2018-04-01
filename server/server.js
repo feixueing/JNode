d3e13d44854ffa75d3b1cfd0c6383288fab94b6a
lgr <632232810@qq.com>
2018-04-01 15:56:10 +0800

    add devSSR

--- server/server.js
+++ server/server.js
@@ -3,20 +3,30 @@ const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
 const favicon = require('serve-favicon')
-const serverEntry = require('../dist/server-entry.js').default
+
+
+const isDev = process.env.NODE_ENV === 'development'
 
 const app = express()
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
-
-app.use('/public', express.static(path.join(__dirname, '../dist')))
 
 app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
-app.get('*', (req,res) => {
-  var content = ReactSSR.renderToString(serverEntry)
-  var html = template.replace('<!-- app -->', content)
-  res.send(html)
-})
+if (isDev) {
+  const serverEntry = require('../dist/server-entry.js').default
+  const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+
+  app.use('/public', express.static(path.join(__dirname, '../dist')))
+
+  app.get('*', (req,res) => {
+    var content = ReactSSR.renderToString(serverEntry)
+    var html = template.replace('<!-- app -->', content)
+    res.send(html)
+  })
+} else {
+  const devStatic = require('./util/dev-static')
+
+  devStatic(app)
+}
 
 app.listen(3333, () => {
   console.log('server is listening on 3333')
